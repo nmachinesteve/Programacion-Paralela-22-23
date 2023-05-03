9dd4f8088f4a096f20e2beb48a4b88c6ffd0b961
nmachinesteve <124213743+nmachinesteve@users.noreply.github.com>
2023-05-03 09:39:30 +0100

    Add files via upload

---
+++ Ejercicios Matt/2Numeros.py
@@ -0,0 +1,62 @@
+# Ejercicio 2: Numeros
+
+from paho.mqtt.client import Client
+from multiprocessing import Process, Manager 
+from time import sleep
+import random
+import sys
+
+NUMBERS = 'numbers'
+CLIENTS = 'clients'
+TIMER_STOP = f'{CLIENTS}/timerstop' 
+HUMIDITY = 'humidity'
+
+def is_prime(n): 
+    i= 2
+    while i*i < n and n % i != 0: i += 1
+    return i*i > n
+
+def timer(time, data): 
+    mqttc = Client()
+    mqttc.connect(data['broker'])
+    msg = f'timer working. timeout: {time}'
+    print(msg) 
+    mqttc.publish(TIMER_STOP, msg) 
+    sleep(time)
+    msg = f'timer working. timeout: {time}' 
+    mqttc.publish(TIMER_STOP, msg)
+    print('timer end working') 
+    mqttc.disconnect()
+
+def on_message(mqttc, data, msg):
+    
+    try:
+        if is_prime(int(msg.payload)):
+            print (f"The number {int(msg.payload)} is prime")
+        else:
+            print (f"The number {int(msg.payload)} is not prime")
+
+    except ValueError as e:
+        print(e)
+        pass
+
+def on_log(mqttc, userdata, level, string):
+    print("LOG", userdata, level, string)
+
+def main(broker):
+    data = {'client': None,'broker': broker}
+    mqttc = Client(userdata=data) 
+    data['client'] = mqttc
+    mqttc.enable_logger()
+    mqttc.on_message = on_message
+    mqttc.on_log = on_log
+    mqttc.connect(broker)
+    mqttc.subscribe(NUMBERS)
+    mqttc.loop_forever()
+
+
+if __name__ == '__main__':
+    hostname = 'simba.fdi.ucm.es'
+    if len(sys.argv)>1:
+        hostname = sys.argv[1]
+    main(hostname)
---
+++ Ejercicios Matt/5Temporizador.py
@@ -0,0 +1,41 @@
+from paho.mqtt.client import Client
+from multiprocessing import Process, Manager 
+from time import sleep
+import paho.mqtt.publish as publish
+import time
+import sys
+
+def on_message(mqttc, data, msg):
+    print(f"MESSAGE:data:{data}, msg.topic:{msg.topic}, payload:{msg.payload}")
+
+def on_log(mqttc, userdata, level, string): 
+    print("LOG", userdata, level, string)
+
+def main(broker):
+    data = {'status':0}
+    mqttc = Client(userdata=data) 
+    mqttc.enable_logger() 
+    mqttc.on_message = on_message 
+    mqttc.on_log = on_log 
+    mqttc.connect(broker)
+
+    res_topics = ['clients/a', 'clients/b']
+    for t in res_topics:
+        mqttc.subscribe(t)
+    mqttc.loop_start()
+    tests = [
+        (res_topics[0], 4, 'one'),
+        (res_topics[1], 1, 'two'),
+        (res_topics[0], 2, 'three'),
+        (res_topics[1], 5, 'four')
+    ]
+    topic = 'clients/timeout' 
+    for test in tests:
+        mqttc.publish(topic, f'{test[0]},{test[1]},{test[2]}')
+    time.sleep(10)
+
+if __name__ == '__main__':
+    hostname = 'simba.fdi.ucm.es'
+    if len(sys.argv)>1:
+        hostname = sys.argv[1]
+    main(hostname)
